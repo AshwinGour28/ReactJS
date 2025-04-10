7af4ce777cf5a2d92a5f154df695c22a9728023d
AshwinGour28 <ashwing2884@gmail.com>
2025-04-10 13:03:53 +0530

    created  a react+vite app and added a basic component

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./Component"
+
+function App() {
+
+  return (
+    <>
+    <h1>React + Vite</h1>
+    <Component />
+    </>
+  )
+}
+
+export default App
---
+++ 01viteReact/src/Component.jsx
@@ -0,0 +1,7 @@
+function Component(){
+    return(
+        <h3>This is a Component</h3>
+    )
+}
+
+export default Component;
